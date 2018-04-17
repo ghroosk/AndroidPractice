1fed4cbadff3071bee6ab9169a348bd20cd31687
ghroosk <zhenghxexplore@gmail.com>
2018-04-17 15:54:17 +0800

    test the commit name

--- app/src/main/java/com/zhx/practice/MainActivity.java
+++ app/src/main/java/com/zhx/practice/MainActivity.java
@@ -1,13 +1,14 @@
 package com.zhx.practice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("");
         setContentView(R.layout.activity_main);
     }
 }
